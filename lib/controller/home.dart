c4d8e66c68a2be24b71510adca7eebecc3020141
DZOIBO IVAN <ivandztitan@gmail.com>
2024-07-31 13:30:17 +0200

    feat: initialize app

---
+++ lib/controller/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar());
+  }
+}
