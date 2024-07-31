a5f7b0755c2305a8cb1bd742c6632aae110b9c26
DZOIBO IVAN <ivandztitan@gmail.com>
2024-07-31 20:11:58 +0200

    feat: implement Ui home

--- lib/controller/home.dart
+++ lib/controller/home.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
 
+import '../model/bar_choice.dart';
+
 class Home extends StatefulWidget {
   @override
   HomeState createState() => HomeState();
 }
 
 class HomeState extends State<Home> {
+  int currentIndex = 0;
+  List<BarChoice> myItems = [
+    BarChoice(label: "Music", iconData: Icons.music_note, page: Container()),
+    BarChoice(label: "Favoris", iconData: Icons.whatshot, page: Container()),
+    BarChoice(label: "Search", iconData: Icons.search, page: Container())
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(100),
+          child: SafeArea(
+              child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Spacer(),
+                          Icon(Icons.radio),
+                          Icon(Icons.doorbell),
+                          Icon(Icons.settings),
+                        ],
+                      ),
+                      Text("My music app"),
+                    ],
+                  )))),
+      body: const Center(child: Text("Mon App")),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: myItems.map((i) => i.item).toList(),
+        onTap: barTapped,
+      ),
+    );
+  }
+
+  barTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
   }
 }
---
+++ lib/model/bar_choice.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BarChoice {
+  String label;
+  IconData iconData;
+  Widget page;
+
+  Icon get icon => Icon(iconData);
+  BottomNavigationBarItem get item =>
+      BottomNavigationBarItem(icon: icon, label: label);
+
+  BarChoice({required this.label, required this.iconData, required this.page});
+}
